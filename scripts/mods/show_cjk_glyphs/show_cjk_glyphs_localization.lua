d6bf351330132a5e2ee0bb36b32553e023291947
xsSplater - СанСаныч <xssplater@gmail.com>
2024-10-14 16:43:15 +0300

    RU show_cjk_glyphs_localization.lua

--- scripts/mods/show_cjk_glyphs/show_cjk_glyphs_localization.lua
+++ scripts/mods/show_cjk_glyphs/show_cjk_glyphs_localization.lua
@@ -2,29 +2,36 @@ return {
     mod_name = {
         en = "Show CJK Glyphs",
         ["zh-cn"] = "显示 CJK 字形",
+        ru = "Показывать китайско-японско-корейские символы",
     },
     mod_description = {
         en = "Show CJK glyphs.",
         ["zh-cn"] = "显示 CJK 字形。",
+        ru = "Show CJK Glyphs - Показывает китайско-японско-корейские символы, не меняя основной язык игры.",
     },
     lang_priority = {
         en = "Language Priority",
         ["zh-cn"] = "语言优先级",
+        ru = "Приоритет языка",
     },
     q_zh_hans = {
         en = "Chinese (Simplified)",
         ["zh-cn"] = "中文（简体）",
+        ru = "Китайский (упрощенный)",
     },
     q_zh_hant = {
         en = "Chinese (Traditional)",
         ["zh-cn"] = "中文（繁体）",
+        ru = "Китайский (традиционный)",
     },
     q_ja = {
         en = "Japanese",
         ["zh-cn"] = "日文",
+        ru = "Японский",
     },
     q_ko = {
         en = "Korean",
         ["zh-cn"] = "韩文",
+        ru = "Корейский",
     },
 }
